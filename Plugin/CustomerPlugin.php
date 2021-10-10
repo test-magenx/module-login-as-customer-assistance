e4ceda81b0e0094855bc4e9c49c9e16b2739deef
root <george.shytikov@icfnext.com>
2021-10-10 23:24:29 +0000

    Magento Fork initial commit

--- Plugin/CustomerPlugin.php
+++ Plugin/CustomerPlugin.php
@@ -7,11 +7,8 @@ declare(strict_types=1);
 
 namespace Magento\LoginAsCustomerAssistance\Plugin;
 
-use Magento\Authorization\Model\UserContextInterface;
 use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Customer\Api\Data\CustomerInterface;
-use Magento\Framework\App\ObjectManager;
-use Magento\Framework\AuthorizationInterface;
 use Magento\LoginAsCustomerAssistance\Api\SetAssistanceInterface;
 use Magento\LoginAsCustomerAssistance\Model\IsAssistanceEnabled;
 
@@ -26,28 +23,12 @@ class CustomerPlugin
     private $setAssistance;
 
     /**
-     * @var AuthorizationInterface
-     */
-    private $authorization;
-
-    /**
-     * @var UserContextInterface
-     */
-    private $userContext;
-
-    /**
      * @param SetAssistanceInterface $setAssistance
-     * @param AuthorizationInterface|null $authorization
-     * @param UserContextInterface|null $userContext
      */
     public function __construct(
-        SetAssistanceInterface $setAssistance,
-        ?AuthorizationInterface $authorization = null,
-        ?UserContextInterface $userContext = null
+        SetAssistanceInterface $setAssistance
     ) {
         $this->setAssistance = $setAssistance;
-        $this->authorization = $authorization ?? ObjectManager::getInstance()->get(AuthorizationInterface::class);
-        $this->userContext = $userContext ?? ObjectManager::getInstance()->get(UserContextInterface::class);
     }
 
     /**
@@ -64,16 +45,9 @@ class CustomerPlugin
         CustomerInterface $result,
         CustomerInterface $customer
     ): CustomerInterface {
-        $enoughPermission = true;
-        if ($this->userContext->getUserType() === UserContextInterface::USER_TYPE_ADMIN
-            || $this->userContext->getUserType() === UserContextInterface::USER_TYPE_INTEGRATION
-        ) {
-            $enoughPermission = $this->authorization->isAllowed('Magento_LoginAsCustomer::allow_shopping_assistance');
-        }
         $customerId = (int)$result->getId();
         $customerExtensionAttributes = $customer->getExtensionAttributes();
-
-        if ($enoughPermission && $customerExtensionAttributes && $customerExtensionAttributes->getAssistanceAllowed()) {
+        if ($customerExtensionAttributes && $customerExtensionAttributes->getAssistanceAllowed()) {
             $isEnabled = (int)$customerExtensionAttributes->getAssistanceAllowed() === IsAssistanceEnabled::ALLOWED;
             $this->setAssistance->execute($customerId, $isEnabled);
         }
